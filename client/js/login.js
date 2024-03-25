317c1df874c578f9902e50950504de7f4f4bf956
MCGruhler <MCGruhler@gmail.com>
2024-03-25 15:00:55 -0400

    add login verification client

---
+++ client/js/login.js
@@ -0,0 +1,34 @@
+$("#loginBut").click(function () {
+  let email = $("#email").val();
+  let pWord = $("#pword").val();
+
+  clickLogIn(email, pWord);
+});
+
+function clickLogIn(email, pWord) {
+  console.log("before ajax call");
+  $.ajax({
+    url: "http://localhost:3000/login",
+    type: "POST",
+    data: {
+      email: email,
+      pWord: pWord,
+    },
+    success: function (res) {
+      console.log(res.msg);
+      window.location.href = "/home";
+    },
+    error: function (res) {
+      if (res.status == 400) {
+        alert("All fields are required");
+      }
+      if (res.status == 401) {
+        alert("Incorrect Password");
+      }
+      if (res.status == 404) {
+        alert("Email not found");
+      }
+      console.log(res.status);
+    },
+  });
+}
