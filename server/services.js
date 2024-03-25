f8cbe34faa766135f1be0d2120f7b4e1c82cd76c
MCGruhler <MCGruhler@gmail.com>
2024-03-25 15:00:30 -0400

    add login verification to server

--- server/services.js
+++ server/services.js
@@ -9,6 +9,7 @@ const ObjectId = require("mongodb").ObjectId;
 const dbURL = "mongodb://localhost:27017/";
 
 let services = function (app) {
+  //for registration--------------------------------------------
   app.post("/register", function (req, res) {
     //let search = { email: req.body.email };
     //console.log(search);
@@ -27,15 +28,75 @@ let services = function (app) {
     const userArr = Array.from(Object.values(reviewUserData));
     const userEmpty = (userInfo) => userInfo != "";
 
+    const user = { email: req.body.email };
+
     MongoClient.connect(dbURL, { useUnifiedTopology: true }).then((client) => {
       if (!userArr.every(userEmpty)) {
         return res.status(400).send(JSON.stringify({ msg: "err" }));
       }
-      // if (emailLen.length > 0) {
-      // return res.status(409).send(JSON.stringify({ msg: "err" }));
-      //}
-      client.db("fitTrackDB").collection("users").insertOne(reviewUserData);
-      return res.status(200).send(JSON.stringify({ msg: "success" }));
+      //finding if an user exsits
+      const userExsits = async function findAUser() {
+        try {
+          const result = await client
+            .db("fitTrackDB")
+            .collection("users")
+            .findOne(user);
+          console.log(result);
+          return result;
+        } catch (error) {}
+      };
+      userExsits().then((result) => {
+        if (result != null) {
+          //checking and returning error if email already exsists
+          return res.status(409).send(JSON.stringify({ msg: "error" }));
+        } else {
+          //return success
+          client.db("fitTrackDB").collection("users").insertOne(reviewUserData);
+          return res.status(200).send(JSON.stringify({ msg: "success" }));
+        }
+      });
+    });
+  });
+  //for login----------------------------------------------------
+  app.post("/login", function (req, res) {
+    const reviewUserData = {
+      email: req.body.email,
+      pWord: req.body.pWord,
+    };
+
+    const userArr = Array.from(Object.values(reviewUserData));
+    const userEmpty = (userInfo) => userInfo != "";
+
+    const userEmail = { email: req.body.email };
+
+    MongoClient.connect(dbURL, { useUnifiedTopology: true }).then((client) => {
+      if (!userArr.every(userEmpty)) {
+        return res.status(400).send(JSON.stringify({ msg: "err" }));
+      }
+
+      const emailFind = async function findUserOne() {
+        try {
+          const result = await client
+            .db("fitTrackDB")
+            .collection("users")
+            .findOne(userEmail);
+          console.log(result);
+          return result;
+        } catch (error) {}
+      };
+      emailFind().then((result) => {
+        if (result != null) {
+          savedPword = result.pWord;
+          if (savedPword == reviewUserData.pWord) {
+            return res.status(200).send(JSON.stringify({ msg: "success" }));
+          } else {
+            //send if password does not match
+            return res.status(401).send(JSON.stringify({ msg: "error" }));
+          }
+        } else {
+          return res.status(404).send(JSON.stringify({ msg: "error" }));
+        }
+      });
     });
   });
 };
